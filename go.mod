f44db68b76612d9a84003b88a7a40b22bf1e8dfd
Moni <1098264843@qq.com>
2026-05-14 14:25:48 +0800

    feat: hello

---
+++ go.mod
@@ -0,0 +1,3 @@
+module test_4_trae
+
+go 1.25.6
--- hello.go
+++ hello.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println("Hello, World!")
+}
+
+func mergeList(l1, l2 []int) []int {
+	i1, i2 := 0, 0
+	len1, len2 := len(l1), len(l2)
+	maxLen := max(len1, len2)
+
+	res := make([]int, 0, len1+len2)
+	for i1 < maxLen && i2 < maxLen {
+		if l1[i1] < l2[i2] {
+			res = append(res, l1[i1])
+			if i1 < len1 {
+				i1++
+			}
+		} else if l1[i1] > l2[i2] {
+			res = append(res, l2[i2])
+			if i2 < len2 {
+				i2++
+			}
+		} else {
+			res = append(res, l1[i1])
+			res = append(res, l2[i2])
+			if i1 < len1 {
+				i1++
+			}
+			if i2 < len2 {
+				i2++
+			}
+		}
+	}
+
+	return res
 }
